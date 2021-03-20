033a1b48091f181e349f00674a5e3a954eef23bd
yooncastle <dbstjd427@gmail.com>
2021-03-20 11:38:35 +0900

    Use google map API

--- sea-tracer/src/App.vue
+++ sea-tracer/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <google-map></google-map>
   </div>
 </template>
+<script>
+export default {
+  name: "App",
+  data () {
+    return {
 
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- sea-tracer/src/main.js
+++ sea-tracer/src/main.js
@@ -2,10 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import GoogleMap from './GoogleMap'
+
+window.EventBus = new Vue({
+  data () {
+    return {
+      sanfrancisco: [37.78268, - 122.41136]
+    }
+  }
+});
+
+Vue.component('GoogleMap', GoogleMap);
 
 Vue.config.productionTip = false
 
 new Vue({
+
   router,
   store,
   render: h => h(App)
