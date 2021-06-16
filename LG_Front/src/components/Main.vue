0fa6f56f0115c6f4a2b95f024736cfabfd02f22b
yooncastle <dbstjd427@gmail.com>
2021-06-17 03:20:00 +0900

    marker Customizing

--- LG_Front/src/components/Main.vue
+++ LG_Front/src/components/Main.vue
@@ -58,6 +58,7 @@ export default {
       devs.forEach((dev, i) => {
         console.log(dev.devName);
         const el = document.createElement("div");
+        el.textContent = dev.devName;
         el.setAttribute("data-marker-index", i);
 
         const t = new CustomMarker(
@@ -87,5 +88,52 @@ export default {
 }
 .item {
   width: 50%;
+}
+</style>
+
+<style lang="scss">
+.custom-marker {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translate(-50%, -100%);
+  background-color: white;
+  padding: 0 8px;
+  border-radius: 28px;
+  box-shadow: rgba(0, 0, 0, 0.08) 0px 0px 0px 1px,
+    rgba(0, 0, 0, 0.18) 0px 1px 2px;
+  color: #222;
+  overflow-y: auto;
+  height: 28px;
+  line-height: 28px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: transform 0.15s ease-in-out;
+  font-size: 14px;
+}
+.custom-marker:hover {
+  transform: translate(-50%, -100%) scale(1.2);
+}
+
+.custom-marker-anchor {
+  position: absolute;
+  width: 100%;
+  bottom: 8px;
+  left: 0;
+}
+
+.custom-marker-container {
+  cursor: auto;
+  height: 0;
+  position: absolute;
+  width: 200px;
+  &.active {
+    .custom-marker {
+      z-index: 1000;
+      background-color: #f7530e;
+      color: #fff;
+      transform: translate(-50%, -100%) scale(1.1);
+    }
+  }
 }
 </style>
